cfe3065143fc4d7dc58866ba5c0a94d303edf346
llaoj <llaoj@users.noreply.github.com>
2025-01-07 09:46:19 +0000

    deploy: 8632d0cd58dcb11cb766f8b0a2053d0604bf582d

--- posts/2208/kubernetes-etcdctl-usage/etcdctl.sh
+++ posts/2208/kubernetes-etcdctl-usage/etcdctl.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir auger
+curl -o ./auger/auger_1.0.2_linux_amd64.tar.gz https://llaoj.oss-cn-beijing.aliyuncs.com/files/github.com/etcd-io/auger/releases/download/v1.0.2/auger_1.0.2_linux_amd64.tar.gz
+tar -xvf ./auger/auger_1.0.2_linux_amd64.tar.gz -C ./auger
+mv ./auger/auger /usr/local/bin/
+rm -rf ./auger
+
 ENDPOINTS=$(ps -ef | grep kube-apiserver | grep -P 'etcd-servers=(.*?)\s' -o | awk -F= '{print $2}')
 CACERT=$(ps -ef | grep kube-apiserver | grep -P 'etcd-cafile=(.*?)\s' -o | awk -F= '{print $2}')
 CERT=$(ps -ef | grep kube-apiserver | grep -P 'etcd-certfile=(.*?)\s' -o | awk -F= '{print $2}')
